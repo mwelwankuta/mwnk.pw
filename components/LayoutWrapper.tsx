2d8f3bd7bad98ae6914a9369551e504845bd25bb
Daniel M. Matongo <m_matongo@icloud.com>
2022-02-22 14:30:07 +0000

    fix build error

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -31,7 +31,7 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex flex-col justify-between h-screen">
         <header
           className={`w-full sticky z-20 top-0 flex items-center justify-between py-4  ${
-            isScrollTop ? 'border-none': 'border-b border-gray-200 dark:border-gray-800'
+            isScrollTop ? 'border-none' : 'border-b border-gray-200 dark:border-gray-800'
           } bg-white dark:bg-black bg-opacity-30 dark:bg-opacity-30 backdrop-filter backdrop-saturate-150 backdrop-blur-lg firefox:bg-opacity-100 dark:firefox:bg-opacity-100`}
         >
           <div className="w-full max-w-2xl mx-auto px-4 sm:px-6 sm:py-2 xl:max-w-3xl xl:px-0 flex items-center justify-between">
