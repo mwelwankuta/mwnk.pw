dc7f9f2bd9492c701950dcc99c7f26dc265ff4e3
Daniel M. Matongo <m_matongo@icloud.com>
2022-02-22 15:36:11 +0000

    fixes

--- next.config.js
+++ next.config.js
@@ -31,7 +31,6 @@ module.exports = withBundleAnalyzer({
     if (!dev && !isServer) {
       // Replace React with Preact only in client production build
       Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
         'react-dom/test-utils': 'preact/test-utils',
         'react-dom': 'preact/compat',
       })
