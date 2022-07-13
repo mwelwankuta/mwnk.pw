5807ec86f78761e6486db84ad0f131a9615e00ee
Daniel M. Matongo <mmatongo_@hotmail.com>
2022-07-13 23:53:01 +0200

    update

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -1,3 +1,9 @@
-export default function SectionContainer({ children }) {
-  return <div className="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-3xl xl:px-0">{children}</div>
+import { Container } from '@/lib/types/common'
+
+export default function SectionContainer({ children }: Container) {
+  return (
+    <div className="max-w-2xl px-4 mx-auto sm:px-6 xl:max-w-3xl xl:px-0 flex flex-col gap-10 sm:gap-14">
+      {children}
+    </div>
+  )
 }
