cfbb869f2aba685de53f9ee675650af3913ed86a
Daniel M. Matongo <mmatongo_@hotmail.com>
2022-07-13 23:50:49 +0200

    update

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,14 +1,13 @@
-import Link from 'next/link'
-
 import Timeline from '@/components/Timeline'
 import Container from '@/components/Container'
+import PageTitle from '@/components/PageTitle'
 
 export default function About() {
   return (
     <Container title="About – Daniel M. Matongo">
       <div className="flex flex-col justify-center items-start px-2 mx-auto mb-10">
-        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
-          About Me
+        <h1 className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <PageTitle>About Me</PageTitle>
         </h1>
         <div className="mb-8 leading-6 text-gray-600 dark:text-gray-400">
           <p>
