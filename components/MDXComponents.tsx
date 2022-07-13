1d479ab9e6806a055eb4bb5ce3ed8e8e5167b0e3
Daniel M. Matongo <mmatongo_@hotmail.com>
2022-07-13 23:52:05 +0200

    updates

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -5,21 +5,25 @@ import Image from './Image'
 import CustomLink from './Link'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
-import { BlogNewsletterForm } from './NewsletterForm'
+import PostLayout from '@/layouts/PostLayout'
 
 export const MDXComponents = {
   Image,
   TOCInline,
   a: CustomLink,
   pre: Pre,
-  BlogNewsletterForm: BlogNewsletterForm,
-  wrapper: ({ components, layout, ...rest }) => {
-    const Layout = require(`../layouts/${layout}`).default
-    return <Layout {...rest} />
-  },
+  wrapper: PostLayout,
 }
 
-export const MDXLayoutRenderer = ({ layout, mdxSource, ...rest }) => {
+export const MDXLayoutRenderer = ({
+  layout,
+  mdxSource,
+  ...rest
+}: {
+  layout: any
+  mdxSource: string
+  [key: string]: any
+}) => {
   const MDXLayout = useMemo(() => getMDXComponent(mdxSource), [mdxSource])
 
   return <MDXLayout layout={layout} components={MDXComponents} {...rest} />
